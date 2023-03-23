16eef938c715cf7733d2d6e79584ffc663c50355
Peter Pilgrim <peter.pilgrim@gmail.com>
2023-03-23 21:01:54 +0000

    Rename thee test class for blog video https://youtu.be/vkWlEQdeuVY Interview Question 2 Find Duplicate Numbers and then Sort

--- src/test/java/org/example/maus/DuplicateIntegersFintechJobSoftwareTest.java
+++ src/test/java/org/example/maus/DuplicateIntegersFintechJobSoftwareTest.java
@@ -16,7 +16,7 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class DuplicateIntegersSantanderAutoSoftwareTest {
+public class DuplicateIntegersFintechJobSoftwareTest {
 
     @DisplayName("Given a set of integer When find duplicate integers Then return sorted set")
     @Test
@@ -25,6 +25,40 @@ public class DuplicateIntegersSantanderAutoSoftwareTest {
         assertThat(text, is(notNullValue()));
         assertThat(text, is("4, 7"));
     }
+
+    @DisplayName("Given a set of integer When find duplicate integers Then return sorted set 2")
+    @Test
+    public void search_for_duplicates_2() {
+        var text = search_duplicates_sort_them(List.of(3,3,3,3,3,3));
+        assertThat(text, is(notNullValue()));
+        assertThat(text, is("3"));
+    }
+
+    @DisplayName("Given a set of integer When find duplicate integers Then return sorted set 3")
+    @Test
+    public void search_for_duplicates_3() {
+        var text = search_duplicates_sort_them(List.of(3, 2, 1, 4, 5, 6, 7, 8, 9, 3, 2, 1));
+        assertThat(text, is(notNullValue()));
+        assertThat(text, is("1, 2, 3"));
+    }
+
+    @DisplayName("Given a set of integer When find duplicate integers Then return sorted set 4")
+    @Test
+    public void search_for_duplicates_4() {
+        var text = search_duplicates_sort_them(List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 6, 4, 2));
+        assertThat(text, is(notNullValue()));
+        assertThat(text, is("2, 4, 6"));
+    }
+
+    @DisplayName("Given a set of integer When find duplicate integers Then return sorted set 5")
+    @Test
+    public void search_for_duplicates_5() {
+        var text = search_duplicates_sort_them(List.of());
+        assertThat(text, is(notNullValue()));
+        assertThat(text, is(""));
+    }
+
+
 
     @DisplayName("Validate Search for Duplicate Integers")
     @ParameterizedTest(name = "{index} ==> expected ''{0}'' from list {1}")
@@ -44,29 +78,40 @@ public class DuplicateIntegersSantanderAutoSoftwareTest {
                 arguments("2, 4, 6", List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 6, 4, 2)),
                 arguments("1, 3, 6, 9", List.of(3, 2, 6, 1, 5, 9, 6, 3, 9, 4, 6, 6, 1)),
                 arguments("", List.of())
-                );
+        );
     }
+
+
+
+
+
+
+
+
     public String search_duplicates_sort_them(List<Integer> numberList) {
-        var histograph = new HashMap<Integer, Integer>();
+
+        var histogram = new HashMap<Integer, Integer>();
         numberList.stream().forEach(digit -> {
-                    if (histograph.containsKey(digit)) {
-                        histograph.put(digit, histograph.get(digit) + 1);
+                    if (histogram.containsKey(digit)) {
+                        histogram.put(digit, histogram.get(digit) + 1);
                     } else {
-                        histograph.put(digit, 1);
+                        histogram.put(digit, 1);
                     }
                 }
         );
 
-        System.out.printf("histograph=%s\n", histograph);
+        System.out.printf("histogram=%s\n", histogram);
 
-        var duplicates = histograph.entrySet().stream().filter(tuple -> {
-            return tuple.getValue() > 1;
-        }).map(tuple -> tuple.getKey()).collect(Collectors.toList());
+        var duplicates = histogram.entrySet().stream().filter(
+                tuple -> {
+                    return tuple.getValue() > 1;
+                }).map(tuple -> tuple.getKey()).collect(Collectors.toList());
         Collections.sort(duplicates);
 
         System.out.printf("duplicates=%s\n", duplicates);
 
         var buf = new StringBuilder();
+        // concatenate the each of the duplicates into a text buffer with a comma
         for (int i = 0; i < duplicates.size(); i++) {
             if (i != 0) {
                 buf.append(", ");
@@ -75,5 +120,6 @@ public class DuplicateIntegersSantanderAutoSoftwareTest {
         }
         System.out.printf(">>>> buf='%s'\n\n", buf.toString());
         return buf.toString();
+
     }
 }
